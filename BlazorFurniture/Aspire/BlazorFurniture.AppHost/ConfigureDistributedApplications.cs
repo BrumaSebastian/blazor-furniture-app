2d9dd84076e7ea7cf28ad6941eb4202d0405977a
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-09-14 20:32:02 +0300

    update to net10

--- BlazorFurniture/Aspire/BlazorFurniture.AppHost/ConfigureDistributedApplications.cs
+++ BlazorFurniture/Aspire/BlazorFurniture.AppHost/ConfigureDistributedApplications.cs
@@ -7,67 +7,66 @@ namespace BlazorFurniture.AppHost;
 
 internal static class ConfigureDistributedApplications
 {
-    public static IDistributedApplicationBuilder AddKeycloak(this IDistributedApplicationBuilder applicationBuilder)
+    extension(IDistributedApplicationBuilder applicationBuilder)
     {
-        var databaseOptions = applicationBuilder.Configuration.GetSection("KeycloakDatabase").Get<KeycloakDatabaseOptions>()
-            ?? throw new InvalidOperationException("Missing keycloak database configuration");
-
-        var userName = applicationBuilder.AddParameter(nameof(databaseOptions.User), databaseOptions.User, true);
-        var password = applicationBuilder.AddParameter(nameof(databaseOptions.Password), databaseOptions.Password, true);
-        var postgresContainer = applicationBuilder.AddPostgres(databaseOptions.ContainerName)
-            .WithUserName(userName)
-            .WithPassword(password)
-            .WithImage(databaseOptions.Image)
-            .WithVolume("postgres_data", databaseOptions.VolumePath)
-            .WithHostPort(databaseOptions.HostPort)
-            .WithLifetime(ContainerLifetime.Persistent)
-            .AddDatabase(databaseOptions.DatabaseName);
-
-        var options = applicationBuilder.Configuration.GetSection("Keycloak").Get<KeycloakOptions>()
-            ?? throw new InvalidOperationException("Missing keycloak configuration");
-
-        string dbUrl = string.IsNullOrWhiteSpace(options.DatabaseURL) 
-            ? $"jdbc:postgresql://{databaseOptions.ContainerName}:{databaseOptions.HostPort}/{databaseOptions.DatabaseName}"
-            : options.DatabaseURL;
-        var keycloakContainer = applicationBuilder.AddContainer(options.ContainerName, options.Image)
-                .WithKeycloakAdminAccount(options.AdminUsername, options.AdminPassword)
-                .WithKeycloakDatabase(options.DatabaseType, dbUrl, options.DatabaseUsername, options.DatabasePassword)
-                .WithArgs(options.Args)
-                .WithHttpEndpoint(options.HostPort, options.ContainerPort)
-                .WithLifetime(ContainerLifetime.Persistent)
-                .WithReference(postgresContainer);
-
-        if (applicationBuilder.Environment.IsDevelopment())
+        public void AddKeycloak()
         {
-            foreach (var providerPath in options.Providers.Where(p => !string.IsNullOrWhiteSpace(p)))
+            var databaseOptions = applicationBuilder.Configuration.GetSection("KeycloakDatabase").Get<KeycloakDatabaseOptions>()
+                ?? throw new InvalidOperationException("Missing keycloak database configuration");
+
+            var userName = applicationBuilder.AddParameter(nameof(databaseOptions.User), databaseOptions.User, true);
+            var password = applicationBuilder.AddParameter(nameof(databaseOptions.Password), databaseOptions.Password, true);
+            var postgresContainer = applicationBuilder.AddPostgres(databaseOptions.ContainerName)
+                .WithUserName(userName)
+                .WithPassword(password)
+                .WithImage(databaseOptions.Image)
+                .WithVolume("postgres_data", databaseOptions.VolumePath)
+                .WithHostPort(databaseOptions.HostPort)
+                .WithLifetime(ContainerLifetime.Persistent)
+                .AddDatabase(databaseOptions.DatabaseName);
+
+            var options = applicationBuilder.Configuration.GetSection("Keycloak").Get<KeycloakOptions>()
+                ?? throw new InvalidOperationException("Missing keycloak configuration");
+
+            string dbUrl = string.IsNullOrWhiteSpace(options.DatabaseURL)
+                ? $"jdbc:postgresql://{databaseOptions.ContainerName}:{databaseOptions.HostPort}/{databaseOptions.DatabaseName}"
+                : options.DatabaseURL;
+            var keycloakContainer = applicationBuilder.AddContainer(options.ContainerName, options.Image)
+                    .WithKeycloakAdminAccount(options.AdminUsername, options.AdminPassword)
+                    .WithKeycloakDatabase(options.DatabaseType, dbUrl, options.DatabaseUsername, options.DatabasePassword)
+                    .WithArgs(options.Args)
+                    .WithHttpEndpoint(options.HostPort, options.ContainerPort)
+                    .WithLifetime(ContainerLifetime.Persistent)
+                    .WithReference(postgresContainer);
+
+            if (applicationBuilder.Environment.IsDevelopment())
             {
-                keycloakContainer = keycloakContainer.WithBindMount(providerPath, $"/opt/keycloak/providers/{Path.GetFileName(providerPath)}");
+                foreach (var providerPath in options.Providers.Where(p => !string.IsNullOrWhiteSpace(p)))
+                {
+                    keycloakContainer = keycloakContainer.WithBindMount(providerPath, $"/opt/keycloak/providers/{Path.GetFileName(providerPath)}");
+                }
             }
         }
 
-        return applicationBuilder;
-    }
+        public void AddMaildev()
+        {
+            var options = applicationBuilder.Configuration.GetSection("Maildev").Get<MaildevOptions>()
+                ?? throw new InvalidOperationException("Missing maildev configuration");
 
-    public static IDistributedApplicationBuilder AddMaildev(this IDistributedApplicationBuilder applicationBuilder)
-    {
-        var options = applicationBuilder.Configuration.GetSection("Maildev").Get<MaildevOptions>()
-            ?? throw new InvalidOperationException("Missing maildev configuration");
-
-        applicationBuilder.AddContainer(options.ContainerName, options.Image)
-            .WithEnvironment("MAILDEV_SMTP_PORT", options.Ports.HostSmtp.ToString())
-            .WithEnvironment("MAILDEV_WEB_PORT", options.Ports.HostWeb.ToString())
-            .WithEndpoint(options.Ports.HostSmtp, options.Ports.ContainerSmtp, name: MaildevOptions.SmtpEndpointName)
-            .WithUrlForEndpoint(MaildevOptions.SmtpEndpointName, e =>
-            {
-                e.DisplayText = MaildevOptions.SmtpEndpointName;
-            })
-            .WithHttpEndpoint(options.Ports.HostWeb, options.Ports.ContainerWeb, name: MaildevOptions.WebEndpointName)
-            .WithUrlForEndpoint(MaildevOptions.WebEndpointName, e =>
-            {
-                e.DisplayText = MaildevOptions.WebEndpointName;
-            })
-            .WithLifetime(ContainerLifetime.Persistent);
-
-        return applicationBuilder;
+            applicationBuilder.AddContainer(options.ContainerName, options.Image)
+                .WithEnvironment("MAILDEV_SMTP_PORT", options.Ports.HostSmtp.ToString())
+                .WithEnvironment("MAILDEV_WEB_PORT", options.Ports.HostWeb.ToString())
+                .WithEndpoint(options.Ports.HostSmtp, options.Ports.ContainerSmtp, name: MaildevOptions.SmtpEndpointName)
+                .WithUrlForEndpoint(MaildevOptions.SmtpEndpointName, e =>
+                {
+                    e.DisplayText = MaildevOptions.SmtpEndpointName;
+                })
+                .WithHttpEndpoint(options.Ports.HostWeb, options.Ports.ContainerWeb, name: MaildevOptions.WebEndpointName)
+                .WithUrlForEndpoint(MaildevOptions.WebEndpointName, e =>
+                {
+                    e.DisplayText = MaildevOptions.WebEndpointName;
+                })
+                .WithLifetime(ContainerLifetime.Persistent);
+        }
     }
 }
