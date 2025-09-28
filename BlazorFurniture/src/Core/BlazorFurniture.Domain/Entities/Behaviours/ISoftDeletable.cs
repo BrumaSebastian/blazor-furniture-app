8f8f4e88cfdc33c96e2516324b61656715a12c4d
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-09-28 22:06:18 +0300

    add behaviour interfaces

---
+++ BlazorFurniture/src/Core/BlazorFurniture.Domain/Entities/Behaviours/ISoftDeletable.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorFurniture.Domain.Entities.Behaviours;
+
+public interface ISoftDeletable
+{
+    bool IsDeleted { get; set; }
+    DateTime? DeletedAt { get; set; }
+    string? DeletedBy { get; set; }
+}
---
+++ BlazorFurniture/src/Core/BlazorFurniture.Domain/Entities/Behaviours/ITrackable.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorFurniture.Domain.Entities.Behaviours;
+
+public interface ITrackable
+{
+    DateTime CreatedAt { get; set; }
+    DateTime? ModifiedAt { get; set; }
+    string CreatedBy { get; set; }
+    string? ModifiedBy { get; set; }
+}
