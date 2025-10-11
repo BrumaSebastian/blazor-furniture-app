f3b03ac5729f3e015c8ca87173822c3bdad04fed
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-10-11 21:29:40 +0300

    add a redirect to keycloak reset password action

---
+++ BlazorFurniture/src/Presentation/BlazorFurniture/Constants/Oidc.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorFurniture.Constants;
+
+public static class Oidc
+{
+    public class Actions
+    {
+        public const string KEYCLOAK_ACTIONS = "kc_action";
+        public const string UPDATE_PASSWORD = "UPDATE_PASSWORD";
+    }
+}
---
+++ BlazorFurniture/src/Presentation/BlazorFurniture/Controllers/User/ResetPasswordEndpoint.cs
@@ -0,0 +1,52 @@
+﻿using BlazorFurniture.Application.Features.UserManagement.Responses;
+using BlazorFurniture.Constants;
+using BlazorFurniture.Extensions;
+using BlazorFurniture.Extensions.Endpoints;
+using FastEndpoints;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Http.Extensions;
+
+namespace BlazorFurniture.Controllers.User;
+
+public class ResetPasswordEndpoint() : EndpointWithoutRequest<UserProfileResponse>
+{
+    public override void Configure()
+    {
+        Get("reset-password");
+        Group<UserEndpointGroup>();
+        Summary(options =>
+        {
+            options.Summary = "Reset password";
+            options.Description = "Redirecting to oidc provider and process the action of resetting the password.";
+            options.Response<UserProfileResponse>(StatusCodes.Status302Found);
+        });
+
+        Description(options =>
+        {
+            options.WithDescription("Redirecting to oidc provider and process the action of resetting the password.");
+            options.WithDisplayName("Reset password");
+            options.WithTags(ControllerTags.User);
+            options.Produces<UserProfileResponse>(StatusCodes.Status302Found);
+        });
+    }
+
+    public override async Task HandleAsync( CancellationToken ct )
+    {
+        var returnUrl = UriHelper.BuildAbsolute(
+            HttpContext.Request.Scheme,
+            HttpContext.Request.Host,
+            HttpContext.Request.PathBase,
+            "/"
+        );
+
+        var props = new AuthenticationProperties { RedirectUri = returnUrl };
+        props.Items[Oidc.Actions.KEYCLOAK_ACTIONS] = Oidc.Actions.UPDATE_PASSWORD;
+
+        await Send.ResultAsync(TypedResults.Challenge(
+            props,
+            [CookieAuthenticationDefaults.AuthenticationScheme, OpenIdConnectDefaults.AuthenticationScheme]
+        ));
+    }
+}
