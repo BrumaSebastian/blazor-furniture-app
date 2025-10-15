3e1b07871dfaf6958e9c82602c3b8aad53dc5652
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-10-15 23:49:25 +0300

    add pagination query param and response

---
+++ BlazorFurniture/src/Core/BlazorFurniture.Application/Common/Requests/QueryParams/PaginationQueryParam.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorFurniture.Application.Common.Requests.QueryParams;
+
+public sealed class PaginationQueryParam
+{
+    public int Page
+    {
+        get;
+        set => field = value < 1 ? 1 : value;
+    }
+
+    public int PageSize
+    {
+        get;
+        set => field = value < 1 ? 10 : Math.Min(value, 100);
+    }
+}
---
+++ BlazorFurniture/src/Core/BlazorFurniture.Application/Common/Responses/PaginatedResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorFurniture.Application.Common.Responses;
+
+public sealed class PaginatedResponse<T> where T : class
+{
+    public int Total { get; set; }
+    public required IEnumerable<T> Results { get; set; }
+}
