8e7b780229ece62a0fab617ce3cbbc99a73850d0
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-10-26 09:56:24 +0200

    78 remove default values for filter

--- BlazorFurniture/src/Core/BlazorFurniture.Application/Features/GroupManagement/Requests/Filters/GroupQueryFilters.cs
+++ BlazorFurniture/src/Core/BlazorFurniture.Application/Features/GroupManagement/Requests/Filters/GroupQueryFilters.cs
@@ -4,7 +4,7 @@ namespace BlazorFurniture.Application.Features.GroupManagement.Requests.Filters;
 
 public record class GroupQueryFilters : IPaginationQueryParams
 {
-    public int? Page { get; init; } = 0;
-    public int? PageSize { get; init; } = 10;
+    public int? Page { get; init; }
+    public int? PageSize { get; init; }
     public string? Name { get; set; }
 }
