4886615ca8ec373a8b59610b66d11feb34c6f679
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-10-30 22:12:11 +0200

    84 investigate rendermodes

---
+++ BlazorFurniture/src/Presentation/BlazorFurniture.Shared/Constants/RenderModes.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorFurniture.Shared.Constants;
+
+public static class RenderModes
+{
+    public static IComponentRenderMode InteractiveWebAssemblyWithoutPrerendering { get; } =
+    new InteractiveWebAssemblyRenderMode(prerender: false);
+
+    public static IComponentRenderMode InteractiveAutoWithoutPrerendering { get; } =
+        new InteractiveAutoRenderMode(prerender: false);
+
+    public static IComponentRenderMode InteractiveServerWithoutPrerendering { get; } =
+        new InteractiveServerRenderMode(prerender: false);
+}
