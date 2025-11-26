d293d8a65707543320d1500e55fd9ba6c3aa9c71
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-11-26 20:53:07 +0200

    implement get users endpoint into the dialog

--- BlazorFurniture/src/Presentation/BlazorFurniture.Shared/Models/Users/UserModel.cs
+++ BlazorFurniture/src/Presentation/BlazorFurniture.Shared/Models/Users/UserModel.cs
@@ -1,9 +1,11 @@
 ﻿namespace BlazorFurniture.Shared.Models.Users;
 
-public class UserModel
+public sealed class UserModel
 {
     public Guid Id { get; set; }
     public required string Email { get; set; }
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
+    public PlatformRoles Role { get; set; }
+    public IEnumerable<Guid> Groups { get; set; } = [];
 }
--- BlazorFurniture/src/Presentation/BlazorFurniture.Shared/Services/API/Interfaces/IUsersApi.cs
+++ BlazorFurniture/src/Presentation/BlazorFurniture.Shared/Services/API/Interfaces/IUsersApi.cs
@@ -1,9 +1,11 @@
-﻿using BlazorFurniture.Shared.Models.Users.Responses;
+﻿using BlazorFurniture.Shared.Models;
+using BlazorFurniture.Shared.Models.Users;
+using BlazorFurniture.Shared.Models.Users.Responses;
 using Refit;
 
 namespace BlazorFurniture.Shared.Services.API.Interfaces;
 
-public interface IUserApi
+public interface IUsersApi
 {
     [Get("/api/user/permissions")]
     Task<UserPermissionsModel> GetUserPermissions(CancellationToken ct);
@@ -13,4 +15,7 @@ public interface IUserApi
 
     [Get("/api/users/{userId}/groups")]
     Task<IApiResponse<List<UserGroupMembershipModel>>> GetUserGroups( Guid userId, CancellationToken ct );
+
+    [Get("/api/users")]
+    Task<IApiResponse<PaginatedModel<UserModel>>> GetUsers( int page, int pageSize, string? search, CancellationToken ct );
 }
