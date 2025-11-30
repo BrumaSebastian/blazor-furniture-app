34b1cbb1a220622e5dbdd07820c6c8056e0ab1b6
Bruma Sebastian <sebastianbruma24@gmail.com>
2025-11-30 16:02:34 +0200

    add change avatar dialog and update profile form

---
+++ BlazorFurniture/src/Presentation/BlazorFurniture.Shared/Models/Users/Requests/UpdateUserProfileModel.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorFurniture.Shared.Models.Users.Requests;
+
+public sealed class UpdateUserProfileModel
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    public string? Email { get; set; }
+}
